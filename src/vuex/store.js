9f9680017faa5187c5bd945d3d661b41f4f6a5c1
liuxb <liuxb@mail.las.ac.cn>
2021-04-15 20:01:25 +0800

    add getters

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -24,13 +24,21 @@ const actions = {
     console.log(context)
     return context.commit('mutationsAddCount', n)
   },
-  actionsReduceCount({ commit }, n = 0) {
+  actionsReduceCount({commit}, n = 0) {
     return commit('mutationsReduceCount', n)
+  }
+}
+
+const getters = {
+  getterCount(state, n = 0) {
+    return (state.count += n)
   }
 }
 
 export default new Vuex.Store({
   state,
   mutations,
-  actions
+  actions,
+  getters
 })
+
